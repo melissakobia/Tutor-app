82e89d9ca0250be2af0af2d2b7062fd2659e8a2b
melissa kobia <melissakobia1997@gmail.com>
2019-11-11 08:22:15 +0300

    Adds homepage logic

--- app/src/main/java/com/example/tutor4you/MainActivity.java
+++ app/src/main/java/com/example/tutor4you/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.tutor4you;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.buttonStudent) Button mButtonStudent;
+    @BindView(R.id.buttonTutor) Button mButtonTutor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mButtonTutor.setOnClickListener(this);
+        mButtonStudent.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mButtonTutor) {
+            Intent intent =new Intent(MainActivity.this, ProfileActivity.class);
+            startActivity(intent);
+        }
+        else if (v == mButtonStudent) {
+            Intent intent = new Intent(MainActivity.this, CategoryListActivity.class);
+            startActivity(intent);
+        }
+
     }
 }
