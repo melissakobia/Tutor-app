505f1c78849d09458cd1e82099cfd0c7c5ee426e
melissa kobia <melissakobia1997@gmail.com>
2019-11-19 09:04:12 +0300

    Updates the main activity to include new layout

--- app/src/main/java/com/example/tutor4you/ui/MainActivity.java
+++ app/src/main/java/com/example/tutor4you/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.tutor4you;
+package com.example.tutor4you.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,10 +7,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.tutor4you.LoginActivity;
+import com.example.tutor4you.R;
+import com.shantanudeshmukh.linkedinsdk.LinkedInBuilder;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final int LINKEDIN_REQUEST = 100;
+    public static String clientID;
+    public static String clientSecret;
+    public static String redirectUrl;
+
+
+
     @BindView(R.id.buttonStudent) Button mButtonStudent;
     @BindView(R.id.buttonTutor) Button mButtonTutor;
 
@@ -20,6 +39,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        getCredentials();
+
         mButtonTutor.setOnClickListener(this);
         mButtonStudent.setOnClickListener(this);
     }
@@ -27,13 +48,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         if (v == mButtonTutor) {
-            Intent intent =new Intent(MainActivity.this, ProfileActivity.class);
+            LinkedInBuilder.getInstance(MainActivity.this)
+                    .setClientID(clientID)
+                    .setClientSecret(clientSecret)
+                    .setRedirectURI(redirectUrl)
+                    .authenticate(LINKEDIN_REQUEST);
+
+
+            Intent intent =new Intent(MainActivity.this, TutorDashboardActivity.class);
             startActivity(intent);
         }
         else if (v == mButtonStudent) {
             Intent intent = new Intent(MainActivity.this, CategoryListActivity.class);
             startActivity(intent);
         }
+
+    }
+
+
+
+    private void getCredentials() {
+        try {
+
+            InputStream is = getAssets().open("linkedin-credentials.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            String json = new String(buffer, "UTF-8");
+            JSONObject linkedinCred = new JSONObject(json);
+            clientID = linkedinCred.getString("client_id");
+            clientSecret = linkedinCred.getString("client_secret");
+            redirectUrl = linkedinCred.getString("redirect_url");
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
