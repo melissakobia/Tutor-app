bb4d8c3abe0c623121295777836ee8dc8da3cccf
melissa kobia <melissakobia1997@gmail.com>
2019-11-19 09:07:06 +0300

    Adds all current changes

--- app/src/androidTest/java/com/example/tutor4you/ProfileActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/example/tutor4you/ProfileActivityInstrumentationTest.java
@@ -4,13 +4,14 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 
+import com.example.tutor4you.ui.ProfileActivity;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
