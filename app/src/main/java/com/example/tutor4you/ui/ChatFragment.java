c41cd6b4f5d1a9d7741f35bde57e3ef2b50958a8
melissa kobia <melissakobia1997@gmail.com>
2019-12-01 17:41:43 +0300

    Adds a fragment to implement chats

---
+++ app/src/main/java/com/example/tutor4you/ui/ChatFragment.java
@@ -0,0 +1,32 @@
+package com.example.tutor4you.ui;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.tutor4you.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ChatFragment extends Fragment {
+
+
+    public ChatFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chat, container, false);
+    }
+
+}
